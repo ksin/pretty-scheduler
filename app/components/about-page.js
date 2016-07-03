4080b781a3495b7e8b686572941f87a995bf20d6
Ken Sin <kensin1129@gmail.com>
2016-07-03 18:39:22 -0400

    Add about page info

--- app/components/about-page.js
+++ app/components/about-page.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showAbout: false,
 
+  icon: Ember.computed('showAbout', function() {
+    return this.get('showAbout') ? "×" : "?";
+  }),
+
+  iconClass: Ember.computed('showAbout', function() {
+    if (this.get('showAbout')) {
+      return 'about__icon about__icon--close';
+    }
+    return 'about__icon about__icon--open';
+  }),
+
+  actions: {
+    toggleAbout() {
+      this.toggleProperty('showAbout');
+    }
+  }
 });
