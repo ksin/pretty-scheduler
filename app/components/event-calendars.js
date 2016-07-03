72541c22d2269e86ca52e6f5288ff37db5e7257c
Ken Sin <kensin1129@gmail.com>
2016-07-03 01:23:34 -0400

    More styling for events page.
    Most of new attendees positioning and sizes are complete.

--- app/components/event-calendars.js
+++ app/components/event-calendars.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isShowingNewAttendeeCalendar: true,
 
+  hasAttendees: Ember.computed.notEmpty('event.attendees'),
+
   actions: {
     toggleCalendars() {
       this.toggleProperty('isShowingNewAttendeeCalendar');
--- app/components/new-attendee-form.js
+++ app/components/new-attendee-form.js
@@ -7,9 +7,7 @@ export default Ember.Component.extend({
   }),
 
   isValidAttendee: Ember.computed('name,secret,isValidSecret', function() {
-    return Ember.isPresent(this.get('name')) &&
-            Ember.isPresent(this.get('secret')) &&
-            this.get('isValidSecret');
+    return Ember.isPresent(this.get('name')) && this.get('isValidSecret');
   }),
 
   attendeeObject: Ember.computed('name,availableDates.[]', function() {
@@ -20,18 +18,28 @@ export default Ember.Component.extend({
     };
   }),
 
+  displayHelloMessage: Ember.computed.and('helloImg', 'helloText'),
+
   actions: {
     createAttendee() {
       if (this.get('isValidAttendee')) {
         this.sendAction('createAttendee', this.get('attendeeObject'));
       } else {
+        debugger;
         console.log('Attendee is invalid. Implement a validation error.');
       }
     },
 
     focusOutNameInput(name) {
-      let helloMessage = Ember.isPresent(name) ? `Hi there, ${name}!` : '';
-      this.set('helloMessage', helloMessage);
+      let helloText, helloImg;
+      if (Ember.isPresent(name)) {
+        helloText = `Hi there, ${name}!`;
+        helloImg = "http://i.giphy.com/26Fxy3Iz1ari8oytO.gif";
+      } else {
+        helloText, helloImg = null;
+      }
+      this.set('helloImg', helloImg);
+      this.set('helloText', helloText);
       this.set('name', name);
     },
 
