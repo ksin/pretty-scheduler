e8e29367702c1ddfd965c39b102499d566049c98
Ken Sin <kensin1129@gmail.com>
2016-07-04 01:15:42 -0400

    Further styling. New event page composition is done.

--- app/components/new-event-form.js
+++ app/components/new-event-form.js
@@ -1,18 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isValidEvent: Ember.computed('name,startDate,endDate,isValidDateRange', function() {
-    return Ember.isPresent(this.get('name')) &&
-            Ember.typeOf(this.get('startDate')) === 'date' &&
-            Ember.typeOf(this.get('endDate')) === 'date' &&
-            this.get('isValidDateRange');
+  error: null,
+
+  isValidEvent: Ember.computed('name,isValidDateRange', function() {
+    if (Ember.isBlank(this.get('name'))) {
+      this.set('error', "What's the event name tho?");
+      return false;
+    } else if (!this.get('isValidDateRange')) {
+      this.set('error', "That date range is invalid.\
+        (You can only create events with a 1-2 month range for now)");
+      return false;
+    }
+    return true;
   }),
 
-  isValidDateRange: Ember.computed('startDate,endDate', function() {
+  isValidDateRange: Ember.computed('startDate,endDate,hasDates', function() {
+    if (!this.get('hasDates')) { return false; }
     let startDate = this.get('startDate');
     let endDate = this.get('endDate');
     let maxDate = new Date(startDate.getFullYear(), startDate.getMonth() + 2, 0);
     return startDate <= endDate && endDate <= maxDate;
+  }),
+
+  hasDates: Ember.computed('startDate,endDate', function() {
+    return Ember.typeOf(this.get('startDate')) === 'date' &&
+            Ember.typeOf(this.get('endDate')) === 'date';
   }),
 
   eventObject: Ember.computed('name,location,details,secret,startDate,endDate', function() {
@@ -35,6 +48,7 @@ export default Ember.Component.extend({
     },
 
     createEvent() {
+      this.set('error', null);
       if (this.get('isValidEvent')) {
         this.sendAction('createEvent', this.get('eventObject'));
       } else {
