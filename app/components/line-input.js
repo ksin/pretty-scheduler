7875d3427a58a025a7229e8c0a4c6d5ae29163c0
Ken Sin <kensin1129@gmail.com>
2016-07-06 23:22:04 -0400

    Don’t make line-input component nested. Just extend TextField. Additional styling throughout.

--- app/components/line-input.js
+++ app/components/line-input.js
@@ -1,18 +1,5 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  indicateNoValue: false,
-  noValueClass: "",
-  classNames: ['line-input'],
-
-  actions: {
-    focusOut(value) {
-      if(this.get('indicateNoValue') && Ember.isBlank(value)) {
-        this.set('noValueClass', " line-input__input--no-value");
-      } else {
-        this.set('noValueClass', "");
-      }
-      this.sendAction('focusOut', value);
-    }
-  }
+export default Ember.TextField.extend({
+  classNames: ['line-input']
 });
