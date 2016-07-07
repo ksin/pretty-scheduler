39f5360876c4d88568d1ee014889088691d5f966
Ken Sin <kensin1129@gmail.com>
2016-07-07 04:48:52 -0400

    Acceptance tests: Update and add new-attendee and attendee-availability.

---
+++ tests/acceptance/attendee-availability-test.js
@@ -0,0 +1,121 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | attendee availability', {
+  beforeEach() {
+    server.create('event', {
+      name: "Chuunin Exam",
+      startDate: new Date(2016, 1, 10),
+      endDate: new Date(2016, 2, 31)}
+    );
+    server.create('attendee', {
+      name: "Ino",
+      eventId: 1,
+      availableDates: [
+        new Date(2016, 1, 10),
+        new Date(2016, 1, 11),
+        new Date(2016, 1, 12)
+    ]});
+    server.create('attendee', {
+      name: "Shika",
+      eventId: 1,
+      availableDates: [
+        new Date(2016, 1, 10),
+        new Date(2016, 1, 11)
+    ]});
+    server.create('attendee', {
+      name: "Cho",
+      eventId: 1,
+      availableDates: [
+        new Date(2016, 1, 10)
+    ]});
+  }
+});
+
+test("cannot view availabilities if no attendees exist for event", function(assert) {
+  server.create('event', {
+    name: "Finding Dory",
+    startDate: new Date(2016, 1, 10),
+    endDate: new Date(2016, 2, 31)}
+  );
+
+  visit('/events/2');
+
+  andThen(() => {
+    assert.equal(find('.event-calendars__toggle').length, 0, "Event calendars toggle is not found");
+  });
+});
+
+test("can view availabilities and revisit new attendee form", function(assert) {
+  visit('/events/1');
+
+  andThen(() => {
+    assert.equal(find('.event-calendars__toggle').text().trim(), "Check out the dates that attendees are available yo!");
+  });
+
+  click('.event-calendars__toggle');
+
+  andThen(() => {
+    assert.equal(find('.event-calendars__toggle').text().trim(), "Hey share the dates that you're available ok?");
+  });
+
+  click('.event-calendars__toggle');
+
+  andThen(() => {
+    assert.equal(find('.new-attendee__header').length, 1, "New attendee header is found");
+    assert.equal(find('.month-calendar__heading--selectable:contains(February 2016)').length, 1, "February selectable calendar is found");
+    assert.equal(find('.month-calendar__heading--selectable:contains(March 2016)').length, 1, "March selectable calendar is found");
+  });
+});
+
+test("renders aggregate attendee availabilities", function(assert) {
+  visit('/events/1');
+
+  click('.event-calendars__toggle');
+
+  andThen(() => {
+    assert.equal(find('.attendees-availabilities__header').text().trim(), "Check out the dates that attendees are available yo!");
+    assert.equal(find('.month-calendar__heading--aggregate:contains(February 2016)').length, 1, "February aggregate calendar is found");
+    assert.equal(find('.month-calendar__heading--aggregate:contains(March 2016)').length, 1, "March aggregate calendar is found");
+    assert.equal(find('.attendees-availabilities__legend').length, 1, "Calendar legend is found");
+    assert.equal(find('.legend').length, 3, "Three keys found in legend");
+    assert.equal(find('.month-calendar__day--everyone:contains(10)').length, 1, "Shows that everyone is available on February 10");
+    assert.equal(find('.month-calendar__day--more:contains(11)').length, 1, "Shows that more are available on February 11");
+    assert.equal(find('.month-calendar__day--few:contains(12)').length, 1, "Shows that few are available on February 12");
+  });
+});
+
+test("renders availability for individual attendee", function(assert) {
+  visit('/events/1');
+
+  click('.event-calendars__toggle');
+
+  andThen(() => {
+    assert.equal(find('.attendee-to-check__attendees').length, 3, "Three attendees listed");
+  });
+
+  click('.attendee-to-check__attendees:contains(Cho)');
+
+  andThen(() => {
+    assert.equal(find('.attendee-to-check__attendees--checking:contains(Cho)').length, 1, "Cho is marked as selected");
+    assert.equal(find('.month-calendar__heading--attendee:contains(February 2016)').length, 1, "February attendee calendar is found");
+    assert.equal(find('.month-calendar__heading--attendee:contains(March 2016)').length, 1, "March attendee calendar is found");
+    assert.equal(find('.month-calendar__day--available:contains(10)').length, 1, "Availability for February 10 is shown");
+  });
+});
+
+test("can re-render aggregate after rendering an individual attendee", function(assert) {
+  visit('/events/1');
+
+  click('.event-calendars__toggle');
+  click('.attendee-to-check__attendees:contains(Cho)');
+  click('.attendee-to-check__all');
+
+  andThen(() => {
+    assert.equal(find('.attendee-to-check__all--checking').length, 1, "All Attendees is marked as selected");
+    assert.equal(find('.month-calendar__heading--aggregate:contains(February 2016)').length, 1, "February aggregate calendar is found");
+    assert.equal(find('.month-calendar__heading--aggregate:contains(March 2016)').length, 1, "March aggregate calendar is found");
+    assert.equal(find('.attendees-availabilities__legend').length, 1, "Calendar legend is found");
+    assert.equal(find('.legend').length, 3, "Three keys found in legend");
+  });
+});
--- tests/acceptance/new-attendee-test.js
+++ tests/acceptance/new-attendee-test.js
@@ -5,8 +5,10 @@ moduleForAcceptance('Acceptance | new attendee', {
   beforeEach() {
     server.create('event', {
       name: "Day of the Black Sun",
+      secret: "roku",
       startDate: new Date(2016, 1, 10),
-      endDate: new Date(2016, 2, 25)});
+      endDate: new Date(2016, 2, 31)}
+    );
   }
 });
 
@@ -19,5 +21,63 @@ test('visiting /events/:id', function(assert) {
     assert.equal(find('.new-attendee__header').length, 1, "New attendee header is found");
     assert.equal(find('.month-calendar__heading--selectable:contains(February 2016)').length, 1, "February selectable calendar is found");
     assert.equal(find('.month-calendar__heading--selectable:contains(March 2016)').length, 1, "March selectable calendar is found");
+  });
+});
+
+test('successfully create attendee with availabilities', function(assert) {
+  visit('/events/1');
+
+  click('.month-calendar__day--selectable:contains(29)'); // February 29
+  click('.month-calendar__day--selectable:contains(31)'); // March 31
+  fillIn('.form-row__input-name', "Steven Universe");
+  fillIn('.form-row__input-secret', "roku");
+  click('.form-row__button');
+
+  andThen(() => {
+    assert.equal(server.db.attendees[0].name, "Steven Universe", "Attendee name is saved to server");
+    assert.deepEqual(server.db.attendees[0].availableDates, [
+      (new Date(2016, 1, 29)).toJSON(), (new Date(2016, 2, 31)).toJSON()
+    ], "Available dates are saved to server");
+    assert.equal(find('.form-message__text').text().trim(), "BOO-YAH! Your dates are saved.");
+    assert.equal(find('.form-message__img').attr('src'), "/assets/images/success.gif");
+    assert.equal(find('.form-message__img').attr('alt'), "GIF of two small round dogs giving each other a paw high five");
+  });
+});
+
+test('says hi when name is entered', function(assert) {
+  visit('/events/1');
+
+  fillIn('.form-row__input-name', "Himawari");
+  triggerEvent('.form-row__input-name', 'blur');
+
+  andThen(() => {
+    assert.equal(find('.form-message__text').text().trim(), "Hi there, Himawari!");
+    assert.equal(find('.form-message__img').attr('src'), "/assets/images/hello.gif");
+    assert.equal(find('.form-message__img').attr('alt'), "GIF of a dog jumping up and waving HI");
+  });
+});
+
+test('missing name', function(assert) {
+  visit('/events/1');
+
+  click('.form-row__button');
+
+  andThen(() => {
+    assert.equal(find('.form-message__text').text().trim(), "What's your name tho?");
+    assert.equal(find('.form-message__img').attr('src'), "/assets/images/no-name.gif");
+    assert.equal(find('.form-message__img').attr('alt'), "GIF of a dog with fire in its eyes");
+  });
+});
+
+test('invalid password', function(assert) {
+  visit('/events/1');
+
+  fillIn('.form-row__input-name', "Aomine");
+  click('.form-row__button');
+
+  andThen(() => {
+    assert.equal(find('.form-message__text').text().trim(), "You got the secret wrong!");
+    assert.equal(find('.form-message__img').attr('src'), "/assets/images/wrong-secret.gif");
+    assert.equal(find('.form-message__img').attr('alt'), "GIF of a dog barking");
   });
 });
